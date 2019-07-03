a8d7b64993a1a5f2462d860890796ac24138ce19
Don Naegely <donald@counsyl.com>
2019-07-02 20:41:43 -0700

    Use Django 2.0+ format_lazy in place of removed string_concat

--- nullablecharfield/widgets.py
+++ nullablecharfield/widgets.py
@@ -1,6 +1,6 @@
 from django.forms.widgets import TextInput
 from django.utils.html import format_html, mark_safe
-from django.utils.translation import string_concat
+from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
 
 class NullableTextWidget(TextInput):
@@ -30,8 +30,29 @@ class NullableTextWidget(TextInput):
         checkbox = format_html('')
         widget_name = 'id_' + name + '_isnull'
         if self.isnull:
-            checkbox = string_concat(' <input type="checkbox" onchange="NullableText.null_event(this)" name="', widget_name,'" id="', widget_name, '" value="1"', checked,'><label class="vCheckboxLabel" for="', widget_name, '">', _('Not specified'),'</label>')
-        return mark_safe(string_concat(super(TextInput, self).render(name, value, attrs), checkbox))
+            # Converted from django.utils.translation.string_concat from:
+            #   https://docs.djangoproject.com/en/2.2/releases/1.11/#id2
+            checkbox_strings = [
+                ' <input type="checkbox" onchange="NullableText.null_event(this)" name="',
+                widget_name,
+                '" id="',
+                widget_name,
+                '" value="1"',
+                checked,
+                '><label class="vCheckboxLabel" for="',
+                widget_name,
+                '">',
+                _('Not specified'),'</label>'
+            ]
+            checkbox = format_lazy('{}' * len(checkbox_strings), *checkbox_strings)
+
+        # Converted from django.utils.translation.string_concat from:
+        #   https://docs.djangoproject.com/en/2.2/releases/1.11/#id2
+        strings = [
+            super(TextInput, self).render(name, value, attrs),
+            checkbox
+        ]
+        return mark_safe(format_lazy('{}' * len(strings), *strings))
 
     def value_from_datadict(self, data, files, name):
         null = data.get(name + '_isnull')
